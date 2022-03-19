c048715c0f2946a7f32d51220e675d22b35570cb
jaimebriesemeister <82653221+jaimebriesemeister@users.noreply.github.com>
2022-03-19 10:56:16 -0300

    Update create_card.py

--- create_card.py
+++ create_card.py
@@ -13,7 +13,7 @@ cv2.putText(img,
            text_to_show,
            (20, 220),  
            fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
-           fontScale=1,  
+           fontScale=0.6,  
            color=(0,0,255)
            )
 
